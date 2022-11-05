7e9a994d91fd9ec98af265e75814da2af672f0a2
ricardouanl <54424491+ricardouanl@users.noreply.github.com>
2022-11-04 19:29:47 -0600

    wip

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function login(){
         user_number: $('#user_number').val(),
         password: $('#password').val()
     }
-
+console.log(params);
     $.post("http://137.184.208.214/sessions", params, function( data ) {
         console.log(data);
         //localStorage.setItem('token', data.token);
